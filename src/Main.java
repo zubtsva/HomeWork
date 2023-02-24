248e39efa88c287a60ba316a465fa273465c4d92
LZ <l.zubtsova@mail.ru>
2023-02-24 10:17:31 +0300

    123

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Зубцова");
+        System.out.println("Лена");
+        System.out.println("Авиадиспетчер");
+        System.out.println("Катание на сноуборде!");
+    }
+}
